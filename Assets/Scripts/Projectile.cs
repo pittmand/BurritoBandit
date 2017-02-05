76d0d3bb90505afdacc49baf5b0586a4aacc48ed
Noy0 <nalov0@gmail.com>
2017-02-04 21:28:15 -0700

    Hot Sauce Projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    float timestamp_Spawn;
+    public float lifespan;
+    public Vector3 direction;
+    public float speed;
+
+	void Start ()
+    {
+        //set timestamp to record age
+        timestamp_Spawn = Time.time;
+
+        //set orientation of projectile
+        transform.rotation = Quaternion.LookRotation(Vector3.up, direction);
+    }
+	
+	void Update ()
+    {
+        if (Time.time > lifespan + timestamp_Spawn)//test if lifespan has elapsed
+            //remove artifact projectile from play
+            Destroy(gameObject);
+        else
+            //apply "physics"
+            transform.position += direction * (Time.deltaTime * speed);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag.Equals("Enemy"))//test if projectile hit an enemy
+        {
+            //damage enemy
+        }
+
+        //projectile is destroyed irregardless on collide
+        Destroy(gameObject);
+    }
+}
