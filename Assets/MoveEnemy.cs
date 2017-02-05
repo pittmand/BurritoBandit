a7c3e38b2bc85dd5425b44fcea482abea6499c14
pittmand <pr0grammer@hotmail.com>
2017-02-04 23:43:08 -0500

    enemy spawn manager- working nav mesh agent.

---
+++ Assets/MoveEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour {
+
+
+    public Transform player;
+    private float moveSpeed = 3;
+
+	void Start () {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Update () {
+        float move = moveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, player.position, move);
+	}
+}
---
+++ Assets/Spawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject[] enemies;
+    public int count;
+    private Vector3 spawnPoint;
+
+	void Update () {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        count = enemies.Length;
+        if (count != 20)
+        {
+            InvokeRepeating("SpawnEnemy", 5, 10);
+        }
+	}
+
+    void SpawnEnemy()
+    {
+        spawnPoint.x = Random.Range(-20, 20);
+        spawnPoint.y = 0.5f;
+        spawnPoint.z = Random.Range(-20,20);
+
+        Instantiate(enemies[UnityEngine.Random.Range(0, enemies.Length - 1)], spawnPoint, Quaternion.identity);
+        CancelInvoke();
+    }
+}
