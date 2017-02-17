b5b0f8686ce76ca359ba31ee02d31a9cb11f916d
Noy0 <nalov0@gmail.com>
2017-02-17 05:12:35 -0700

    Added General Save Data Feature
    + added saving game progress to file
    + added highscore to general save

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class GameController : MonoBehaviour {
 
@@ -22,6 +23,7 @@ public class GameController : MonoBehaviour {
     State currentState;
     bool paused;
     float timestamp_PowerUP;
+    float timestamp_AutoSave;
 
     //objects
     public GameObject mainMenu;
@@ -44,6 +46,14 @@ public class GameController : MonoBehaviour {
     public int max_life = 6;
     public int starting_life = 3;
     public float duration_PowerUP = 10.0f;
+    public bool toggle_AutoSave = true;
+    public float duration_AutoSave = 60.0f;
+
+    //files
+    SaveData saveData;
+
+
+    /// UNITY EVENTS ///
 
     void Start () {
         //set game state
@@ -67,9 +77,53 @@ public class GameController : MonoBehaviour {
         SceneManager.LoadScene("MainMenu");
         mainMenu.SetActive(true);
 
+        //set timestamps
+        timestamp_AutoSave = Time.unscaledTime;
+
+        //load general save data
+        LoadData();
+
         //set game state
         currentState = State.MainMenu;
     }
+
+    void Update()
+    {
+        //check if pauseing
+        if (currentState == State.MainGame && Input.GetButtonDown("Pause"))
+        {
+            if (paused)
+                ContinueGame();
+            else
+                PauseGame();
+        }
+
+        if (!paused)
+        {
+            //check if a power up expired
+            if (power_up && duration_PowerUP < Time.time - timestamp_PowerUP)
+            {
+
+            }
+        }
+
+        //check if beaten highscore
+        if(current_score > saveData.highScore)
+        {
+            saveData.highScore = current_score;
+        }
+
+        //check if time to auto save
+        if (toggle_AutoSave && currentState >= State.MainGame && duration_AutoSave < Time.unscaledTime - timestamp_AutoSave)
+        {
+            AutoSave();
+            timestamp_AutoSave = Time.unscaledTime;
+        }
+    }
+
+
+
+    ///  TRANSITIONS  ///
 
     public void StartIntro()
     {
@@ -108,6 +162,9 @@ public class GameController : MonoBehaviour {
 
     public void QuitGame()
     {
+        //save game
+        SaveData();
+
         Application.Quit();
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
@@ -119,13 +176,16 @@ public class GameController : MonoBehaviour {
         //freeze ingame time (if active)
         Time.timeScale = 0.0f;
 
-        //close hud
-        HUD.SetActive(false);
-
         //close in game menu (if open)
         inGameMenu.SetActive(false);
 
-        //TODO: exit level
+        //close hud
+        HUD.SetActive(false);
+
+        //save game
+        SaveData();
+
+        //exit level
         SceneManager.LoadScene("MainMenu");
 
         //open main menu
@@ -174,28 +234,10 @@ public class GameController : MonoBehaviour {
         settingsMenu.SetActive(false);
     }
 
-    void Update()
-    {
-        //check if pauseing
-        if(currentState == State.MainGame && Input.GetButtonDown("Pause"))
-        {
-            if (paused)
-                ContinueGame();
-            else
-                PauseGame();
-        }
 
-        if (!paused)
-        {
-            //check if a power up expired
-            if (power_up && duration_PowerUP < Time.time - timestamp_PowerUP)
-            {
 
-            }
-        }
 
-        
-    }
+    ///  PLAYER STATUS  ///
 
     void setLife(int value)
     {
@@ -243,5 +285,50 @@ public class GameController : MonoBehaviour {
     {
         power_up = true;
         timestamp_PowerUP = Time.time;
+    }
+
+
+
+
+    ///  SAVE  ///
+
+    internal string save_FilePath()
+    {
+        return Application.persistentDataPath + "/save.json";
+    }
+
+    internal void LoadData()
+    {
+        // Test if file has been created
+        if (File.Exists(save_FilePath()))
+        {
+            // Read file
+            string data = File.ReadAllText(save_FilePath());
+            saveData = JsonUtility.FromJson<SaveData>(data);
+        }
+        else
+        {
+            // Set defaults
+            saveData = new SaveData();
+            saveData.highScore = 0;
+
+            // Create default save file
+            SaveData();
+        }
+
+        
+    }
+
+    internal void SaveData()
+    {
+        Debug.Log("Save");
+        string data = JsonUtility.ToJson(saveData, true);
+        File.WriteAllText(save_FilePath(), data);
+    }
+
+    internal void AutoSave()
+    {
+        Debug.Log("Auto-Save");
+        SaveData();
     }
 }
