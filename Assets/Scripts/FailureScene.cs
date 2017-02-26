93066b06a5b9bed9728e61c992acd2908e42158c
Noy0 <nalov0@gmail.com>
2017-02-25 21:25:44 -0700

    built a you lose scene

--- Assets/Scripts/FailureScene.cs
+++ Assets/Scripts/FailureScene.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class FailureScene : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float duration_FadeIn = 3;
+    public float duration_ExitDelay = 5;
+    private float timestamp_loaded;
+    private bool flag = true;
+    
+    void Start () {
+        timestamp_loaded = Time.unscaledTime;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (flag && Time.unscaledTime >= timestamp_loaded + duration_ExitDelay)
+        {
+            if(Input.GetButton("Submit") || Input.GetButton("Cancel"))
+            {
+                GameController.s_Instance.ReturnToMain();
+                flag = false;
+            }
+        }
 	}
 }
