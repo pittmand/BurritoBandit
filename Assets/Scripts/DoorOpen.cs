72901c3903ddd1179c595e3d6bcea31a71cb7933
pittmand <pr0grammer@hotmail.com>
2017-11-29 21:29:17 -0500

    animation of door
    Added door open/close animation

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    private Animator _animator;
+
+    private bool doorOpen;
+
+    private bool openedOnce = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        doorOpen = false;
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            openedOnce = doorOpen = true;
+            Doors("Open");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (doorOpen)
+        {
+            doorOpen = false;
+            Doors("Close");
+        }
+    }
+
+    void Doors(string direction)
+    {
+        _animator.SetTrigger(direction);
+    }
+}
