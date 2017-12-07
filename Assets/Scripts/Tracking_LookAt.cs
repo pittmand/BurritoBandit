330fc5f76cf02b715ba6eac678d75b5efb1a6a31
Noy0 <nalov0@gmail.com>
2017-12-06 17:30:54 -0700

    reverted incorrectly applied changes
    this isn't the player weapon script but a general look at script.

--- Assets/Scripts/Tracking_LookAt.cs
+++ Assets/Scripts/Tracking_LookAt.cs
@@ -11,14 +11,16 @@ public class Tracking_LookAt : MonoBehaviour {
     public Quaternion rotation;
 
     // Use this for initialization
-    void Start () {
-		if (target == null)
+    void Start()
+    {
+        if (target == null)
             target = GameObject.Find(targetName).transform;
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (target != null)
         {
             if (ignoreYAxis)
@@ -27,30 +29,21 @@ public class Tracking_LookAt : MonoBehaviour {
                 Vector3 targ = new Vector3(target.position.x, 0, target.position.z);
                 Quaternion targetRotation;
                 if (lookAway)
-                {
-                    targetRotation = Quaternion.LookRotation(origin - targ);                    
-                }
+                    targetRotation = Quaternion.LookRotation(origin - targ);
                 else
-                    targetRotation = Quaternion.LookRotation(targ - origin);      
-                          
+                    targetRotation = Quaternion.LookRotation(targ - origin);
                 transform.eulerAngles = Vector3.up * targetRotation.eulerAngles.y;
-                this.transform.localRotation = targetRotation;
-                this.transform.Rotate(90.0f, 0.0f, 0.0f);
+
             }
             else
             {
                 if (lookAway)
-                {
                     transform.rotation = Quaternion.LookRotation(transform.position - target.position);
-                }
                 else
-                {
                     transform.rotation = Quaternion.LookRotation(target.position - transform.position);
-
-                }
             }
         }
-        else if(rotation != Quaternion.identity)
+        else if (rotation != Quaternion.identity)
         {
             transform.rotation = rotation;
         }
