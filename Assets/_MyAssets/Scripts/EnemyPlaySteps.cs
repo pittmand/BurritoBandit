7769eae7e39790c615be09455bd0f3835cf4f24f
DatOneOverbearingGuy <dragonsummoner41@hotmail.com>
2017-12-10 01:01:37 -0600

    Adding Script asset to play sounds on enemies.

---
+++ Assets/_MyAssets/Scripts/EnemyPlaySteps.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class EnemyPlaySteps : MonoBehaviour {
+    private AudioSource[] soundHandler;
+    private AudioSource _stepSound;
+    private int starttime;
+    private int newtime;
+    private int curtime;
+
+    // Use this for initialization
+    void Start () {
+        soundHandler = GetComponents<AudioSource>();
+        _stepSound = soundHandler[1];
+        starttime = (int)Time.time;
+        newtime = starttime + (int)Random.Range(3, 20);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        curtime = (int)Time.time;
+        if (curtime == newtime)
+        {
+            play();
+        }
+
+
+	}
+
+    void play()
+    {
+         _stepSound.Play();
+        gennewtime();
+    }
+
+    void gennewtime() {
+        newtime = curtime + (int)Random.Range(3, 10);
+
+    }
+}
